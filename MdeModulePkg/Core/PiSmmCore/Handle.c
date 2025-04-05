b11211436b04d782383857eea84bbec0cc8ccdc6
wjq <w@w>
2025-04-06 00:29:40 +0200

    fix dispatch protocol bug

--- MdeModulePkg/Core/PiSmmCore/Handle.c
+++ MdeModulePkg/Core/PiSmmCore/Handle.c
@@ -417,7 +417,7 @@ GUID RootHandlerGuids[] = {
 };
 UINTN NumGuidInUse = 0;
 extern BOOLEAN IsRootHandler;
-EFI_STATUS EFIAPI RegisterSmmDispatchHandler(
+EFI_STATUS EFIAPI RegisterDispatchHandler(
   IN  VOID *This,
   IN  VOID *DispatchFunction,
   IN  VOID *RegisterContext,
@@ -433,7 +433,7 @@ EFI_STATUS EFIAPI RegisterSmmDispatchHandler(
   IsRootHandler = FALSE;
   return Status;
 }
-EFI_STATUS EFIAPI UnRegisterSmmDispatchHandler(
+EFI_STATUS EFIAPI UnRegisterDispatchHandler(
   IN CONST VOID    *This,
   IN       EFI_HANDLE                      DispatchHandle
 ) {
@@ -442,9 +442,33 @@ EFI_STATUS EFIAPI UnRegisterSmmDispatchHandler(
 typedef struct _SMM_ROOT_SMI_DISPATCH {
   VOID *Register;
   VOID *UnRegister;
+  UINTN Padding1; // do not remove in case some protocol need it
+  UINTN Padding2; // do not remove in case some protocol need it
+  UINTN Padding3; // do not remove in case some protocol need it
 }SMM_ROOT_SMI_DISPATCH;
-
 SMM_ROOT_SMI_DISPATCH SmmRootSmiDispatch;
+
+typedef struct _SMI_ROOT_SMI_DISPATCH {
+  UINT8 Revision;
+  VOID *UnRegister;
+  VOID *Register1;
+  VOID *Register2;
+  VOID *Register3;
+  VOID *Register4;
+  VOID *Register5;
+  VOID *Register6;
+  VOID *Register7;
+  VOID *Register8;
+  VOID *Register9;
+  VOID *Register10;
+  VOID *Register11;
+  VOID *Register12;
+  VOID *Register13;
+  VOID *Register14;
+}SMI_ROOT_SMI_DISPATCH;
+SMI_ROOT_SMI_DISPATCH SmiRootSmiDispatch;
+
+
 EFI_STATUS
 EFIAPI
 SmmInstallProtocolInterfaceFuzz (
@@ -458,66 +482,97 @@ SmmInstallProtocolInterfaceFuzz (
   if (SmmLocateProtocol(Protocol, NULL, &TmpInterface) == EFI_SUCCESS)
     return EFI_SUCCESS;
 
-  GUID DispatchHandlerGuids[] = {
-    // EFI_SMM_SW_DISPATCH2_PROTOCOL_GUID
+  GUID SmmDispatchHandlerGuids[] = {
+    // EFI_SMM_SW_DISPATCH2_PROTOCOL_GUID-
     { 
       0x18a3c6dc, 0x5eea, 0x48c8, {0xa1, 0xc1, 0xb5, 0x33, 0x89, 0xf9, 0x89, 0x99}},
-    // EFI_SMM_SW_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_SW_DISPATCH_PROTOCOL_GUID-
     { 
       0xe541b773, 0xdd11, 0x420c, {0xb0, 0x26, 0xdf, 0x99, 0x36, 0x53, 0xf8, 0xbf}},
-    // EFI_SMM_SX_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_SX_DISPATCH2_PROTOCOL_GUID-
     { 
       0x456d2859, 0xa84b, 0x4e47, {0xa2, 0xee, 0x32, 0x76, 0xd8, 0x86, 0x99, 0x7d}},
-    // EFI_SMM_SX_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_SX_DISPATCH_PROTOCOL_GUID-
     {
       0x14FC52BE, 0x01DC, 0x426C, {0x91, 0xAE, 0xA2, 0x3C, 0x3E, 0x22, 0x0A, 0xE8}},
-    // EFI_SMM_IO_TRAP_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_IO_TRAP_DISPATCH2_PROTOCOL_GUID-
     {
       0x58DC368D, 0x7BFA, 0x4E77, {0xAB, 0xBC, 0x0E, 0x29, 0x41, 0x8D, 0xF9, 0x30}},
-    // EFI_SMM_IO_TRAP_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_IO_TRAP_DISPATCH_PROTOCOL_GUID-
     {
       0xDB7F536B, 0xEDE4, 0x4714, {0xA5, 0xC8, 0xE3, 0x46, 0xEB, 0xAA, 0x20, 0x1D}},
-    // EFI_SMM_GPI_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_GPI_DISPATCH2_PROTOCOL_GUID-
     {
       0x25566B03, 0xB577, 0x4CBF, {0x95, 0x8C, 0xED, 0x66, 0x3E, 0xA2, 0x43, 0x80}},
-    // EFI_SMM_GPI_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_GPI_DISPATCH_PROTOCOL_GUID-
     {
       0xE0744B81, 0x9513, 0x49CD, {0x8C, 0xEA, 0xE9, 0x24, 0x5E, 0x70, 0x39, 0xDA}},
-    // EFI_SMM_USB_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_USB_DISPATCH2_PROTOCOL_GUID-
     {
         0xEE9B8D90, 0xC5A6, 0x40A2, {0xBD, 0xE2, 0x52, 0x55, 0x8D, 0x33, 0xCC, 0xA1}},
-    // EFI_SMM_USB_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_USB_DISPATCH_PROTOCOL_GUID-
     {
         0xA05B6FFD, 0x87AF, 0x4E42, {0x95, 0xC9, 0x62, 0x28, 0xB6, 0x3C, 0xF3, 0xF3}},
-    // EFI_SMM_STANDBY_BUTTON_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_STANDBY_BUTTON_DISPATCH2_PROTOCOL_GUID-
     {
         0x7300C4A1, 0x43F2, 0x4017, {0xA5, 0x1B, 0xC8, 0x1A, 0x7F, 0x40, 0x58, 0x5B}},
-    // EFI_SMM_STANDBY_BUTTON_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_STANDBY_BUTTON_DISPATCH_PROTOCOL_GUID-
     {
         0x78965B98, 0xB0BF, 0x449E, {0x8B, 0x22, 0xD2, 0x91, 0x4E, 0x49, 0x8A, 0x98}},
-    // EFI_SMM_PERIODIC_TIMER_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_PERIODIC_TIMER_DISPATCH2_PROTOCOL_GUID-
     {
         0x4CEC368E, 0x8E8E, 0x4D71, {0x8B, 0xE1, 0x95, 0x8C, 0x45, 0xFC, 0x8A, 0x53}},
-    // EFI_SMM_PERIODIC_TIMER_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_PERIODIC_TIMER_DISPATCH_PROTOCOL_GUID-
     {
         0x9CCA03FC, 0x4C9E, 0x4A19, {0x9B, 0x06, 0xED, 0x7B, 0x47, 0x9B, 0xDE, 0x55}},
-    // EFI_SMM_POWER_BUTTON_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_POWER_BUTTON_DISPATCH2_PROTOCOL_GUID-
     {
         0x1B1183FA, 0x1823, 0x46A7, {0x88, 0x72, 0x9C, 0x57, 0x87, 0x55, 0x40, 0x9D}},
-    // EFI_SMM_POWER_BUTTON_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_POWER_BUTTON_DISPATCH_PROTOCOL_GUID-
     {
         0xB709EFA0, 0x47A6, 0x4B41, {0xB9, 0x31, 0x12, 0xEC, 0xE7, 0xA8, 0xEE, 0x56}},
-    // EFI_SMM_ICHN_DISPATCH_PROTOCOL_GUID
+    // EFI_SMM_ICHN_DISPATCH_PROTOCOL_GUID-
     {
       0xC50B323E, 0x9075, 0x4F2A, {0xAC, 0x8E, 0xD2, 0x59, 0x6A, 0x10, 0x85, 0xCC}},
-    // EFI_SMM_ICHN_DISPATCH2_PROTOCOL_GUID
+    // EFI_SMM_ICHN_DISPATCH2_PROTOCOL_GUID-
     {
       0xADF3A128, 0x416D, 0x4060, {0x8D, 0xDF, 0x30, 0xA1, 0xD7, 0xAA, 0xB6, 0x99}},
-    // PCH_TCO_SMI_DISPATCH_PROTOCOL_GUID
+    // FCH_SMM_GPI_DISPATCH2_PROTOCOL_GUID-
     {
-      0x9E71D609, 0x6D24, 0x47FD, {0xB5, 0x72, 0x61, 0x40, 0xF8, 0xD9, 0xC2, 0xA4}},
+      0x7051ab6d, 0x9ec2, 0x42eb, {0xa2, 0x13, 0xde, 0x48, 0x81, 0xf1, 0xf7, 0x87}},
+    // FCH_SMM_IO_TRAP_DISPATCH2_PROTOCOL_GUID-
+    {
+      0x91288fc4, 0xe64b, 0x4ef9, {0xa4, 0x63, 0x66, 0x88, 0x0, 0x71, 0x7f, 0xca}},
+    // FCH_SMM_PERIODICAL_DISPATCH2_PROTOCOL_GUID-
+    {
+      0x736102f1, 0x9584, 0x44e7, {0x82, 0x8a, 0x43, 0x4b, 0x1e, 0x67, 0x5c, 0xc4}},
+    // FCH_SMM_PWR_BTN_DISPATCH2_PROTOCOL_GUID
+    {
+      0xa365240e, 0x56b0, 0x426d, {0x83, 0xa, 0x30, 0x66, 0xc6, 0x81, 0xbe, 0x9a}},
+    // FCH_SMM_SW_DISPATCH2_PROTOCOL_GUID-
+    {
+      0x881b4ab6, 0x17b0, 0x4bdf, {0x88, 0xe2, 0xd4, 0x29, 0xda, 0x42, 0x5f, 0xfd}},
+    // FCH_SMM_SX_DISPATCH2_PROTOCOL_GUID-
+    {
+      0x87e2a6cf, 0x91fb, 0x4581, {0x90, 0xa9, 0x6f, 0x50, 0x5d, 0xdc, 0x1c, 0xb2}},
+    // FCH_SMM_USB_DISPATCH_PROTOCOL_GUID-
+    {
+      0x59053b0d, 0xeeb8, 0x4379, {0xb1, 0xc8, 0x14, 0x5f, 0x1b, 0xb, 0xe4, 0xb9}},
+    // FCH_SMM_USB_DISPATCH2_PROTOCOL_GUID-
+    {
+      0xfbbb2ea9, 0xce0e, 0x4689, {0xb3, 0xf0, 0xc6, 0xb8, 0xf0, 0x76, 0xbd, 0x20}},
+    // FCH_SMM_MISC_DISPATCH_PROTOCOL_GUID-
+    {
+      0x13bd659b, 0xb4c6, 0x47da, {0x9b, 0x22, 0x11, 0x50, 0xd4, 0xf3, 0xb, 0xda}},
+    // FCH_SMM_APU_RAS_DISPATCH_PROTOCOL_GUID
+    {
+      0xf871ee59, 0x29d2, 0x4b15, {0x9e, 0x67, 0xaf, 0x32, 0xcd, 0xc1, 0x41, 0x73}},
+  };
+
+  GUID SmiDispatchHandlerGuids[] = {
     // PCH_PCIE_SMI_DISPATCH_PROTOCOL_GUID
-    {0x3E7D2B56, 0x3F47, 0x42AA, {0x8F, 0x6B, 0x22, 0xF5, 0x19, 0x81, 0x8D, 0xAB}},
+    {
+      0x3E7D2B56, 0x3F47, 0x42AA, {0x8F, 0x6B, 0x22, 0xF5, 0x19, 0x81, 0x8D, 0xAB}},
     // PCH_ACPI_SMI_DISPATCH_PROTOCOL_GUID
     {
       0xD52BB262, 0xF022, 0x49EC, {0x86, 0xD2, 0x7A, 0x29, 0x3A, 0x7A, 0x5, 0x4B}},
@@ -530,53 +585,20 @@ SmmInstallProtocolInterfaceFuzz (
     // PCH_ESPI_SMI_DISPATCH_PROTOCOL_GUID
     {
       0xB3C14FF3, 0xBAE8, 0x456C, {0x86, 0x31, 0x27, 0xFE, 0x0C, 0xEB, 0x34, 0x0C}},
-    // EFI_ACPI_EN_DISPATCH_PROTOCOL_GUID
+    // PCH_TCO_SMI_DISPATCH_PROTOCOL_GUID
     {
-      0xBD88EC68, 0xEBE4, 0x4F7B, {0x93, 0x5A, 0x4F, 0x66, 0x66, 0x42, 0xE7, 0x5F}},
-    // EFI_ACPI_DIS_DISPATCH_PROTOCOL_GUID
-    {
-      0x9C939BA6, 0x1FCC, 0x46F6, {0xB4, 0xE1, 0x10, 0x2D, 0xBE, 0x18, 0x65, 0x67}},
-    // FCH_SMM_GPI_DISPATCH2_PROTOCOL_GUID
-    {
-      0x7051ab6d, 0x9ec2, 0x42eb, {0xa2, 0x13, 0xde, 0x48, 0x81, 0xf1, 0xf7, 0x87}},
-    // FCH_SMM_IO_TRAP_DISPATCH2_PROTOCOL_GUID
-    {
-      0x91288fc4, 0xe64b, 0x4ef9, {0xa4, 0x63, 0x66, 0x88, 0x0, 0x71, 0x7f, 0xca}},
-    // FCH_SMM_PERIODICAL_DISPATCH2_PROTOCOL_GUID
-    {
-      0x736102f1, 0x9584, 0x44e7, {0x82, 0x8a, 0x43, 0x4b, 0x1e, 0x67, 0x5c, 0xc4}},
-    // FCH_SMM_PWR_BTN_DISPATCH2_PROTOCOL_GUID
-    {
-      0xa365240e, 0x56b0, 0x426d, {0x83, 0xa, 0x30, 0x66, 0xc6, 0x81, 0xbe, 0x9a}},
-    // FCH_SMM_SW_DISPATCH2_PROTOCOL_GUID
-    {
-      0x881b4ab6, 0x17b0, 0x4bdf, {0x88, 0xe2, 0xd4, 0x29, 0xda, 0x42, 0x5f, 0xfd}},
-    // FCH_SMM_SX_DISPATCH2_PROTOCOL_GUID
-    {
-      0x87e2a6cf, 0x91fb, 0x4581, {0x90, 0xa9, 0x6f, 0x50, 0x5d, 0xdc, 0x1c, 0xb2}},
-    // FCH_SMM_USB_DISPATCH_PROTOCOL_GUID
-    {
-      0x59053b0d, 0xeeb8, 0x4379, {0xb1, 0xc8, 0x14, 0x5f, 0x1b, 0xb, 0xe4, 0xb9}},
-    // FCH_SMM_USB_DISPATCH2_PROTOCOL_GUID
-    {
-      0xfbbb2ea9, 0xce0e, 0x4689, {0xb3, 0xf0, 0xc6, 0xb8, 0xf0, 0x76, 0xbd, 0x20}},
-    // FCH_SMM_MISC_DISPATCH_PROTOCOL_GUID
-    {
-      0x13bd659b, 0xb4c6, 0x47da, {0x9b, 0x22, 0x11, 0x50, 0xd4, 0xf3, 0xb, 0xda}},
-    // FCH_SMM_APU_RAS_DISPATCH_PROTOCOL_GUID
-    {
-      0xf871ee59, 0x29d2, 0x4b15, {0x9e, 0x67, 0xaf, 0x32, 0xcd, 0xc1, 0x41, 0x73}},
+      0x9E71D609, 0x6D24, 0x47FD, {0xB5, 0x72, 0x61, 0x40, 0xF8, 0xD9, 0xC2, 0xA4}},
   };
 
-  SmmRootSmiDispatch.Register = RegisterSmmDispatchHandler;
-  SmmRootSmiDispatch.UnRegister = UnRegisterSmmDispatchHandler;
-  for (UINTN i = 0; i < ( sizeof(DispatchHandlerGuids) / sizeof(DispatchHandlerGuids[0])) ; i++)
+  SmmRootSmiDispatch.Register = RegisterDispatchHandler;
+  SmmRootSmiDispatch.UnRegister = UnRegisterDispatchHandler;
+  for (UINTN i = 0; i < ( sizeof(SmmDispatchHandlerGuids) / sizeof(SmmDispatchHandlerGuids[0])) ; i++)
   {
-    if (CompareGuid(Protocol, &DispatchHandlerGuids[i])) {
+    if (CompareGuid(Protocol, &SmmDispatchHandlerGuids[i])) {
       EFI_HANDLE Handle = NULL;
       EFI_STATUS Status = SmmInstallProtocolInterface (
                   &Handle,
-                  &DispatchHandlerGuids[i],
+                  &SmmDispatchHandlerGuids[i],
                   EFI_NATIVE_INTERFACE,
                   &SmmRootSmiDispatch
                   );
@@ -584,8 +606,39 @@ SmmInstallProtocolInterfaceFuzz (
       (VOID)Status;
       return EFI_SUCCESS;
     }
-    
   }
+  SmiRootSmiDispatch.Register1 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register2 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register3 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register4 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register5 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register6 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register7 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register8 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register9 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register10 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register11 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register12 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register13 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.Register14 = RegisterDispatchHandler;
+  SmiRootSmiDispatch.UnRegister = UnRegisterDispatchHandler;
+  for (UINTN i = 0; i < ( sizeof(SmiDispatchHandlerGuids) / sizeof(SmiDispatchHandlerGuids[0])) ; i++)
+  {
+    if (CompareGuid(Protocol, &SmiDispatchHandlerGuids[i])) {
+      EFI_HANDLE Handle = NULL;
+      EFI_STATUS Status = SmmInstallProtocolInterface (
+                  &Handle,
+                  &SmiDispatchHandlerGuids[i],
+                  EFI_NATIVE_INTERFACE,
+                  &SmiRootSmiDispatch
+                  );
+      ASSERT_EFI_ERROR (Status);
+      (VOID)Status;
+      return EFI_SUCCESS;
+    }
+  }
+
+
   UINT64 OldInFuzz = SmmFuzzGlobalData->in_fuzz;
   SmmFuzzGlobalData->in_fuzz = 0;
   EFI_STATUS Status = SmmInstallProtocolInterface(UserHandle, Protocol, InterfaceType, Interface);
